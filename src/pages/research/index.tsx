faf21d6b7e3cc9a997e00bb3c598fd47e12a1f58
Binita Shrestha <binitastha34@gmail.com>
2024-04-01 12:23:42 +0545

    few changes

--- src/pages/research/index.tsx
+++ src/pages/research/index.tsx
@@ -6,7 +6,7 @@ import Metatag from "@/utils/Metatag";
 export default function Research() {
   return (
     <Layout>
-      <Metatag heading="Nepal Medical College" subheading="Research" description="" og_image="/images/ogImage/homePage.png" />
+      <Metatag heading="Nepal Medical College" subheading="Research" description="" og_image="https://futurehealthcaretoday.com/wp-content/uploads/2021/01/shutterstock_691541095-scaled.jpg" />
       <CommonBanner
         headerName="NMC Research"
         imageLink="/images/Banners/Researchbanner.png"
